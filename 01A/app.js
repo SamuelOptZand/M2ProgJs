7d207af77b8a8924bdcad326b500f900663b2a76
SamuelOptZand <36631@ma-web.nl>
2023-11-24 10:56:50 +0100

    push4

--- 01A/app.js
+++ 01A/app.js
@@ -1,16 +1,23 @@
 class App {
     runApplication() {
-        let appNaam = "gaysex";
-        let versieNummer = 1;
-        let versieDatum = Date=1;
-        let Autheur = "john pork";
-        let copyright = "Samuel";
-        let distributeur = "emporkium";
-        let darkmode = true;
-        console.log("hello world");
-        console.log(appNaam, versieNummer, versieDatum, Autheur, copyright, distributeur, darkmode);
+        this.greeting = "starting up";
+        this.appNaam = "gaysex";
+        this.versieNummer = 1;
+        this.versieDatum = Date=1;
+        this.Autheur = "john pork";
+        this.copyright = "Samuel";
+        this.distributeur = "emporkium";
+        this.darkmode = true;
+
+        this.boolean = true;
+        this.string = "hello world";
+        this.nummer = 4;
     }
 }
 
 let app = new App();
 app.runApplication();
+
+
+console.log(app.greeting, app.appNaam, app.versieNummer, app.versieDatum, app.Autheur, app.copyright, app.distributeur, app.darkmode);
+console.log(app.boolean, app.string, app.nummer);
