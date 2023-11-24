d0d390d95809236eaf5a8248022bf80001aacef9
SamuelOptZand <36631@ma-web.nl>
2023-11-24 10:48:26 +0100

    push3

---
+++ 01B/app.js
@@ -0,0 +1,16 @@
+class App {
+    runApplication() {
+        let appNaam = "gaysex";
+        let versieNummer = 1;
+        let versieDatum = Date=1;
+        let Autheur = "john pork";
+        let copyright = "Samuel";
+        let distributeur = "emporkium";
+        let darkmode = true;
+        console.log("hello world");
+        console.log(appNaam, versieNummer, versieDatum, Autheur, copyright, distributeur, darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
