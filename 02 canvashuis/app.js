80e2bb43fe1cace15b7dedeb66cd2ac7bac5a97b
SamuelOptZand <36631@ma-web.nl>
2023-12-01 10:37:41 +0100

    aaaaaaaaaaaaaaaaa

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -3,8 +3,16 @@ class App {
         
         let canvas = document.getElementById("canvasId");
         console.log(canvas);
+
         let g = canvas.getContext("2d");
+        
+        g.fillStyle = "red";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "black";
         g.fillRect(0,0,10,10);
+    
+    
+    
     }
 }
 
